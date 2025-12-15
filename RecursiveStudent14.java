452b86fbcc738073af313abe810a742bc00452f1
onlyyourza <paseoking456@gmail.com>
2025-12-15 17:20:47 +0700

    2.1	Experiment 1

---
+++ RecursiveStudent14.java
@@ -0,0 +1,22 @@
+public class RecursiveStudent14 {
+    static int factorialRecursive(int n) {
+        if(n==1){
+            return 1;
+        } else {
+            return n * factorialRecursive(n-1);
+        }
+    }
+    static int factorialIterative(int n) {
+        int factorial = 1;
+        for(int i = n; i >= 1; i--) {
+            factorial = factorial * i;
+        }
+        return factorial;
+    }
+    
+    public static void main(String[] args) {
+        System.out.println("Hello world!!!");
+        System.out.println(factorialRecursive(5));
+        System.out.println(factorialIterative(5));
+    }
+}
