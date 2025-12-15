a698ea068e8007e3458a38adac393bebc978b504
onlyyourza <paseoking456@gmail.com>
2025-12-15 17:20:30 +0700

    2.	Add program code to print the power calculation series. Example: calculatePower(2,5) will print 2x2x2x2x2x1 = 32!

--- PowerRecursive14.java
+++ PowerRecursive14.java
@@ -1,19 +1,26 @@
 import java.util.Scanner;
 public class PowerRecursive14 {
     static int calculatePower(int base, int pow) {
-        if(pow==0) {
+        if (pow == 0) {
+            System.out.print("1");
             return 1;
         } else {
+            System.out.print(base + "x");
             return base * calculatePower(base, pow - 1);
         }
     }
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter base number: ");
         int base = input.nextInt();
         System.out.print("Enter power number: ");
         int pow = input.nextInt();
-        System.out.println("Result of " + base + " power "+ pow + " = " + calculatePower(base, pow));
+
+        System.out.print("Result: ");
+        int result = calculatePower(base, pow);
+        System.out.println(" = " + result);
+
         input.close();
     }
 }
